f8661ee82886e208e77209a11c8a42333ee17409
kayanhales <daybreakbliss@gmail.com>
2025-09-22 21:12:42 -0400

    update route.ts

--- app/api/submit-resource/route.ts
+++ app/api/submit-resource/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
     const { data: repoData } = await octokit.repos.get({
       owner: OWNER,
       repo: REPO,
-    });
+    }); 
     const defaultBranch = repoData.default_branch;
 
     // 2️⃣ Create a new branch for this submission
